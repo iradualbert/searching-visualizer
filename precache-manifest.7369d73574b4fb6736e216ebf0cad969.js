834bd03ed30d3367c9c28599ea14810291f290de
Albert Iradukunda <albert.iradukunda@yahoo.com>
2020-09-24 00:13:45 +0300

    Updates

---
+++ precache-manifest.7369d73574b4fb6736e216ebf0cad969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac565d2a5ef51789ce202c6191c1885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f792618bf0adab6bee",
+    "url": "/static/css/main.b5210ba7.chunk.css"
+  },
+  {
+    "revision": "40689b1fac351a9c81d7",
+    "url": "/static/js/2.4712e0b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4712e0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f792618bf0adab6bee",
+    "url": "/static/js/main.d838ef44.chunk.js"
+  },
+  {
+    "revision": "66422d037c297125f0c9",
+    "url": "/static/js/runtime-main.3ffbcf97.js"
+  }
+]);
